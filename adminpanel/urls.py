4db7f203d51ad466b586c406ab6ae58231292f92
Your Name <you@example.com>
2025-10-23 00:17:03 +0800

    basic admin dashboard

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('login/', views.admin_login, name='login'),
     path('signup/', views.admin_signup, name='signup'),
     path('dashboard/', views.admin_dashboard, name='dashboard'),
+    path('customers/', views.admin_customers, name='customers'),
+    path('products/', views.admin_products, name='products'),
+    path('orders/', views.admin_orders, name='orders'),
+    path('logout/', views.admin_logout, name='logout'),
 ]
